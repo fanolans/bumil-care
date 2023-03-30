81d818e4154aeffe060ccd932097774fb61147bf
fanolans <mailenolan@gmail.com>
2023-03-31 01:42:03 +0700

    create profile screen

---
+++ lib/screens/profile_screen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: const BackButton(color: Colors.black),
+        centerTitle: true,
+        title: const Text(
+          'Profil',
+          style: TextStyle(color: Colors.black),
+        ),
+        elevation: 0,
+      ),
+      body: Container(),
+    );
+  }
+}
