a8a0a9a681dbb79405b153edc498cc5081338a94
fanolans <mailenolan@gmail.com>
2023-03-31 01:35:59 +0700

    create auth screen

---
+++ lib/screens/auth_screen.dart
@@ -0,0 +1,87 @@
+import 'package:bumil_care/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+import '../utils/constants.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            width: double.infinity,
+            height: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('${assetImages}background.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: const [
+              Text(
+                'Selamat Datang di',
+                style: TextStyle(color: Colors.white),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                'Bumil Care',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+          Positioned(
+            bottom: 40,
+            child: ElevatedButton.icon(
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(
+                  MediaQuery.of(context).size.width / 1.3,
+                  MediaQuery.of(context).size.height / 15,
+                ),
+                foregroundColor: Colors.black,
+                backgroundColor: Colors.white,
+                shadowColor: Colors.black,
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const HomeScreen(),
+                  ),
+                );
+              },
+              icon: const ImageIcon(
+                AssetImage(
+                  '${assetIcons}icon-google.png',
+                ),
+              ),
+              label: const Text('Masuk dengan Google'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
