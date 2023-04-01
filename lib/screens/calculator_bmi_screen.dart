d01bc1d1fda88c360ce96fed8764d134fe0c7c40
fanolans <mailenolan@gmail.com>
2023-04-01 22:14:01 +0700

    create function and card calculator

--- lib/screens/calculator_bmi_screen.dart
+++ lib/screens/calculator_bmi_screen.dart
@@ -8,6 +8,36 @@ class CalculatorBMIScreen extends StatefulWidget {
 }
 
 class _CalculatorBMIScreenState extends State<CalculatorBMIScreen> {
+  int age = 17;
+  int weight = 50;
+  int height = 180;
+  double maxHeight = 220;
+  double minHeight = 120;
+
+  void ageIncrement() {
+    setState(() {
+      age++;
+    });
+  }
+
+  void ageDecrement() {
+    setState(() {
+      age--;
+    });
+  }
+
+  void weightIncrement() {
+    setState(() {
+      weight++;
+    });
+  }
+
+  void weightDecrement() {
+    setState(() {
+      weight--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,7 +51,222 @@ class _CalculatorBMIScreenState extends State<CalculatorBMIScreen> {
         ),
         elevation: 0,
       ),
-      body: Container(),
+      body: Container(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Card(
+              color: Colors.grey.shade50,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height / 5,
+                width: MediaQuery.of(context).size.width / 1.1,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 5, bottom: 5),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'TINGGI BADAN',
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        '$height',
+                        style: const TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const Text(
+                        'cm',
+                      ),
+                      Slider(
+                        value: height.toDouble(),
+                        min: minHeight,
+                        max: maxHeight,
+                        activeColor: Colors.blue.shade600,
+                        inactiveColor: Colors.grey,
+                        onChanged: (double newValue) {
+                          setState(() {
+                            height = newValue.round();
+                          });
+                        },
+                        semanticFormatterCallback: (double newValue) {
+                          return '$newValue.round()';
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 55,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Card(
+                  color: Colors.grey.shade50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height / 4,
+                    width: MediaQuery.of(context).size.width / 2.4,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'BERAT BADAN',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '$weight',
+                            style: const TextStyle(
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const Text(
+                            'kg',
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              InkWell(
+                                onTap: weightDecrement,
+                                child: Container(
+                                  height: 40.0,
+                                  width: 40.0,
+                                  margin: const EdgeInsets.all(10.0),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.blue.shade600),
+                                  child: const Center(
+                                    child: Text(
+                                      "-",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: 40.0,
+                                width: 40.0,
+                                margin: const EdgeInsets.all(10.0),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.blue.shade600),
+                                child: InkWell(
+                                  onTap: weightIncrement,
+                                  child: const Center(
+                                    child: Text(
+                                      "+",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                Card(
+                  color: Colors.grey.shade50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height / 4,
+                    width: MediaQuery.of(context).size.width / 2.4,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'USIA',
+                            style: TextStyle(
+                              fontSize: 15,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '$age',
+                            style: const TextStyle(
+                              fontSize: 50,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          const Text(
+                            'tahun',
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: <Widget>[
+                              InkWell(
+                                onTap: weightDecrement,
+                                child: Container(
+                                  height: 40.0,
+                                  width: 40.0,
+                                  margin: const EdgeInsets.all(10.0),
+                                  decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(10),
+                                      color: Colors.blue.shade600),
+                                  child: const Center(
+                                    child: Text(
+                                      "-",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                height: 40.0,
+                                width: 40.0,
+                                margin: const EdgeInsets.all(10.0),
+                                decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.circular(10),
+                                    color: Colors.blue.shade600),
+                                child: InkWell(
+                                  onTap: weightIncrement,
+                                  child: const Center(
+                                    child: Text(
+                                      "+",
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
