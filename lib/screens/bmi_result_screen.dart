bc603cb626d548fb889cf1aefab6f33cd6a5f17e
fanolans <mailenolan@gmail.com>
2023-04-04 20:04:44 +0700

    add widget info and recommendation

--- lib/screens/bmi_result_screen.dart
+++ lib/screens/bmi_result_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bumil_care/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 class BMIResultScreen extends StatelessWidget {
@@ -25,25 +26,191 @@ class BMIResultScreen extends StatelessWidget {
         ),
         elevation: 0,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
+      body: Stack(
+        alignment: Alignment.center,
         children: [
-          const Text(
-            'Hasil Perhitungan BMI',
-            textAlign: TextAlign.center,
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            color: Colors.white,
+            child: Column(
+              children: [
+                Card(
+                  color: Colors.blue.shade300,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height / 5,
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 5, bottom: 5),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        textBaseline: TextBaseline.alphabetic,
+                        children: [
+                          const Text(
+                            'BMI Anda',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            bmiResult,
+                            style: const TextStyle(
+                              fontSize: 50,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            resultText,
+                            style: const TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 55,
+                ),
+                Card(
+                  color: Colors.grey.shade50,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height / 5,
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 20),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: const [
+                              Text(
+                                'Kurang dari 18.5',
+                              ),
+                              Text(
+                                'Kurus',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: const [
+                              Text(
+                                '18.5 sampai 24.9',
+                              ),
+                              Text(
+                                'Normal',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: const [
+                              Text(
+                                '25 sampai 29.9',
+                              ),
+                              Text(
+                                'Kegemukan',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: const [
+                              Text(
+                                '25 sampai 29.9',
+                              ),
+                              Text(
+                                'Obesitas',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 55,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.woman_rounded,
+                        size: 50,
+                        color: Colors.blue.shade300,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height / 55,
+                      ),
+                      Text(
+                        recommendation,
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-          Text(
-            resultText.toUpperCase(),
-            textAlign: TextAlign.center,
-          ),
-          Text(
-            bmiResult,
-            textAlign: TextAlign.center,
-          ),
-          Text(
-            recommendation,
-            textAlign: TextAlign.center,
+          Positioned(
+            bottom: 40,
+            child: ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                fixedSize: Size(
+                  MediaQuery.of(context).size.width / 1.3,
+                  MediaQuery.of(context).size.height / 15,
+                ),
+                foregroundColor: Colors.white,
+                backgroundColor: Colors.blue.shade300,
+                shadowColor: Colors.black,
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => HomeScreen(),
+                  ),
+                );
+              },
+              child: const Text('Simpan'),
+            ),
           ),
         ],
       ),
