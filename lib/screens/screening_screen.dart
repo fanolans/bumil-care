1470c3ab2aeba259670111c8b97c9e609180f821
fanolans <mailenolan@gmail.com>
2023-04-04 20:07:13 +0700

    create widget screening and elevated button

--- lib/screens/screening_screen.dart
+++ lib/screens/screening_screen.dart
@@ -21,7 +21,132 @@ class _ScreeningScreenState extends State<ScreeningScreen> {
         ),
         elevation: 0,
       ),
-      body: Container(),
+      body: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            color: Colors.white,
+            width: double.infinity,
+            child: Column(
+              children: [
+                Card(
+                  color: Colors.blue.shade300,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: SizedBox(
+                    height: MediaQuery.of(context).size.height / 5,
+                    width: MediaQuery.of(context).size.width / 1.1,
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 20, vertical: 10),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: const [
+                              Icon(
+                                Icons.insights_rounded,
+                                color: Colors.white,
+                              ),
+                              SizedBox(
+                                width: 5,
+                              ),
+                              Text(
+                                'Sekilas Edukasi',
+                                style: TextStyle(
+                                  fontSize: 15,
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(
+                            height: 5,
+                          ),
+                          const Text(
+                            'isi edukasi',
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 55,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.assignment,
+                        color: Colors.blue.shade300,
+                      ),
+                      SizedBox(
+                        height: MediaQuery.of(context).size.height / 55,
+                      ),
+                      const Text(
+                        'Apakah anda pernah mengalami kehamilan lebih dari 2 kali?',
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            bottom: 40,
+            child: Column(
+              children: [
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width / 1.3,
+                      MediaQuery.of(context).size.height / 15,
+                    ),
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.blue.shade300,
+                    shadowColor: Colors.black,
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Iya'),
+                ),
+                SizedBox(
+                  height: MediaQuery.of(context).size.height / 55,
+                ),
+                ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    fixedSize: Size(
+                      MediaQuery.of(context).size.width / 1.3,
+                      MediaQuery.of(context).size.height / 15,
+                    ),
+                    foregroundColor: Colors.blue.shade300,
+                    backgroundColor: Colors.white,
+                    shadowColor: Colors.black,
+                    elevation: 5,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: const Text('Tidak'),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
