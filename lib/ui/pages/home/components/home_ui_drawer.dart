9bcccfcba4c7612acfb948bd940f5f440198cb9c
fanolans <mailenolan@gmail.com>
2023-05-12 10:59:05 +0700

    create home ui drawer

---
+++ lib/ui/pages/home/components/home_ui_drawer.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../cubit/auth/auth_cubit.dart';
+import '../../login/login_ui.dart';
+import '../../profile/profile_ui.dart';
+
+class HomeDrawer extends StatelessWidget {
+  const HomeDrawer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          DrawerHeader(
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              stops: const [0.0, 1.0],
+              colors: [
+                Colors.red.shade300,
+                Colors.red.shade200,
+              ],
+            )),
+            child: const Text("Header"),
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.person,
+            ),
+            title: const Text(
+              'Profil',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const ProfilePage(),
+                ),
+              );
+            },
+          ),
+          ListTile(
+            leading: const Icon(
+              Icons.exit_to_app,
+            ),
+            title: const Text(
+              'Keluar',
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            onTap: () {
+              context.read<AuthCubit>().signOut();
+
+              // Temporary solution
+              Navigator.pushAndRemoveUntil(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const LoginPage(),
+                ),
+                (route) => false,
+              );
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
