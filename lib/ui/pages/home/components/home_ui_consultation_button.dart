395fc14bce4e7f91ae8545e3afbf93fef592c2d6
fanolans <mailenolan@gmail.com>
2023-05-12 11:01:01 +0700

    create component button

---
+++ lib/ui/pages/home/components/home_ui_consultation_button.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import '../../../../common/constant/string_constant.dart';
+
+class HomeConsultationButton extends StatelessWidget {
+  const HomeConsultationButton({
+    super.key,
+    required this.name,
+    this.onPressed,
+  });
+
+  final String name;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height / 7,
+      width: MediaQuery.of(context).size.width / 2.5,
+      child: ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+          elevation: 3,
+          foregroundColor: Colors.red.shade300,
+          backgroundColor: Colors.white,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+          minimumSize: Size(
+            double.infinity,
+            MediaQuery.of(context).size.height / 5,
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(
+              Icons.chat,
+              size: 20,
+              color: Colors.red.shade300,
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              StringConstant.homeConsultationButtonTitle,
+              style: TextStyle(
+                letterSpacing: 1,
+                fontSize: 14,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            ),
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              StringConstant.homeConsultationButton,
+              textAlign: TextAlign.left,
+              style: TextStyle(
+                letterSpacing: 0,
+                fontSize: 11,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
