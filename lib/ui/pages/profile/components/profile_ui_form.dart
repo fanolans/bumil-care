cb39499d990708231c06c79d7326fa176a787918
fanolans <mailenolan@gmail.com>
2023-05-24 22:54:09 +0700

    remove unused import

--- lib/ui/pages/profile/components/profile_ui_form.dart
+++ lib/ui/pages/profile/components/profile_ui_form.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
-import 'package:form_builder_validators/form_builder_validators.dart';
 import 'package:intl/intl.dart';
 
 import 'package:flutter_quiz/ui/pages/profile/components/profile_ui_label.dart';
