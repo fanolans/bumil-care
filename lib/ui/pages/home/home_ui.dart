0a5dbaa3458f3ae344dc4f312fce3df8bee5551d
fanolans <mailenolan@gmail.com>
2023-05-24 22:53:37 +0700

    add education list and refactor widget

--- lib/ui/pages/home/home_ui.dart
+++ lib/ui/pages/home/home_ui.dart
@@ -1,25 +1,15 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_quiz/cubit/auth/auth_cubit.dart';
 import 'package:flutter_quiz/data/datasources/firebase_database_datasource.dart';
 import 'package:flutter_quiz/data/local/shared_preferences_data.dart';
-import 'package:flutter_quiz/data/models/quiz.dart';
-import 'package:flutter_quiz/ui/pages/login/login_ui.dart';
+import 'package:flutter_quiz/ui/pages/education/education_ui.dart';
 import 'package:flutter_quiz/ui/pages/monitoring/monitoring_ui.dart';
-import 'package:flutter_quiz/ui/pages/profile/profile_ui.dart';
-import 'package:flutter_quiz/ui/pages/quiz/quiz_type_one_ui.dart';
-import 'package:flutter_quiz/ui/pages/quiz/quiz_type_two_ui.dart';
 import 'package:flutter_quiz/ui/pages/quiz/quiz_ui.dart';
 import 'package:flutter_quiz/ui/pages/result/result_ui.dart';
-import 'package:scaffold_gradient_background/scaffold_gradient_background.dart';
 
 import '../../../common/constant/assets_constant.dart';
 import '../../../common/constant/string_constant.dart';
 import '../../../data/models/account.dart';
-import '../education/component/caousel.dart';
-import '../education/education_ui.dart';
 import 'components/home_ui_consultation_button.dart';
 import 'components/home_ui_drawer.dart';
 import 'components/home_ui_screening_button.dart';
@@ -163,257 +153,100 @@ class _HomePageState extends State<HomePage> {
               const SizedBox(
                 height: 35,
               ),
-              Expanded(
-                child: Container(
-                  width: double.infinity,
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(30),
-                      topLeft: Radius.circular(30),
-                    ),
+              Container(
+                width: double.infinity,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(30),
+                    topLeft: Radius.circular(30),
                   ),
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Icon(
-                        Icons.drag_handle_rounded,
-                        color: Colors.grey.shade600,
-                      ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          HomeScreeningButton(
-                            name: StringConstant.homeStartQuiz,
-                            onPressed: () {
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Icon(
+                      Icons.drag_handle_rounded,
+                      color: Colors.grey.shade600,
+                    ),
+                    const SizedBox(
+                      height: 5,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        HomeScreeningButton(
+                          name: StringConstant.homeStartQuiz,
+                          onPressed: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) {
+                                  return const QuizUi();
+                                },
+                              ),
+                            );
+                          },
+                        ),
+                        const SizedBox(
+                          width: 16,
+                        ),
+                        HomeConsultationButton(
+                          name: StringConstant.homeStartQuiz,
+                          onPressed: () async {
+                            if (!await checkEligbleTakeQuiz()) {
+                              // ignore: use_build_context_synchronously
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                const SnackBar(
+                                  backgroundColor: Colors.red,
+                                  content: Text(
+                                      'Lengkapi dulu data pribadi anda di halaman profil'),
+                                ),
+                              );
+                              return;
+                            } else {
+                              // ignore: use_build_context_synchronously
                               Navigator.push(
                                 context,
                                 MaterialPageRoute(
                                   builder: (context) {
-                                    return const QuizUi();
+                                    return const MonitoringPage();
                                   },
                                 ),
                               );
-                            },
-                          ),
-                          const SizedBox(
-                            width: 16,
-                          ),
-                          HomeConsultationButton(
-                            name: StringConstant.homeStartQuiz,
-                            onPressed: () async {
-                              if (!await checkEligbleTakeQuiz()) {
-                                // ignore: use_build_context_synchronously
-                                ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                    backgroundColor: Colors.red,
-                                    content: Text(
-                                        'Lengkapi dulu data pribadi anda di halaman profil'),
-                                  ),
-                                );
-                                return;
-                              } else {
-                                // ignore: use_build_context_synchronously
-                                Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                    builder: (context) {
-                                      return const MonitoringPage();
-                                    },
-                                  ),
-                                );
-                              }
-                            },
-                          ),
-                        ],
-                      ),
-                      const Padding(
-                        padding: EdgeInsets.only(left: 20, top: 20),
-                        child: Align(
-                          alignment: Alignment.centerLeft,
-                          child: Text(
-                            'Edukasi Preeklamsi',
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                    const Padding(
+                      padding: EdgeInsets.only(left: 20, top: 20),
+                      child: Align(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          'Rekomendasi Edukasi',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
                       ),
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      Text('Data')
-                    ],
-                  ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 50,
+                    ),
+                  ],
                 ),
+              ),
+              const Expanded(
+                child: EducationPage(),
               ),
             ],
           ),
         ],
       ),
     );
-    // return ScaffoldGradientBackground(
-    //   drawer: const HomeDrawer(),
-    //   extendBody: true,
-    //   gradient: LinearGradient(
-    //     begin: Alignment.topLeft,
-    //     end: Alignment.bottomRight,
-    //     stops: const [0.0, 1.0],
-    //     colors: [
-    //       Colors.red.shade300,
-    //       Colors.red.shade100,
-    //     ],
-    //   ),
-    //   appBar: AppBar(
-    //     leading: Builder(
-    //       builder: (BuildContext context) {
-    //         return IconButton(
-    //           icon: const Icon(
-    //             Icons.menu_rounded,
-    //             color: Colors.white,
-    //             size: 30,
-    //           ),
-    //           onPressed: () {
-    //             Scaffold.of(context).openDrawer();
-    //           },
-    //           tooltip: MaterialLocalizations.of(context).openAppDrawerTooltip,
-    //         );
-    //       },
-    //     ),
-    //     backgroundColor: Colors.transparent,
-    //     centerTitle: true,
-    //     title: const Text(
-    //       'Bumil Care',
-    //       style: TextStyle(
-    //         color: Colors.white,
-    //         fontWeight: FontWeight.bold,
-    //         fontSize: 20,
-    //       ),
-    //     ),
-    //   ),
-    //   body: SingleChildScrollView(
-    //     scrollDirection: Axis.vertical,
-    //     child: Column(
-    //       children: [
-    //         Padding(
-    //           padding: const EdgeInsets.fromLTRB(20, 0, 20, 0),
-    //           child: Column(
-    //             children: [
-    //               HomeUserProfileCard(
-    //                 name: user.displayName ?? 'User',
-    //                 imagePath: user.photoURL ?? 'https://i.pravatar.cc/300',
-    //               ),
-    //               SizedBox(
-    //                 height: MediaQuery.of(context).size.height / 55,
-    //               ),
-    //               HomeTopButton(
-    //                 name: isLoading ? 'Loading...' : resikoValue,
-    //                 onPressed: () {
-    //                   if (resikoValue == 'Belum ada hasil') {
-    //                     ScaffoldMessenger.of(context).showSnackBar(
-    //                       const SnackBar(
-    //                         backgroundColor: Colors.green,
-    //                         content:
-    //                             Text('Belum ada hasil, silahkan skrining dulu'),
-    //                       ),
-    //                     );
-    //                   } else {
-    //                     Navigator.push(
-    //                       context,
-    //                       MaterialPageRoute(
-    //                         builder: (context) {
-    //                           return const ResultPage();
-    //                         },
-    //                       ),
-    //                     );
-    //                   }
-    //                 },
-    //               ),
-    //             ],
-    //           ),
-    //         ),
-    //         Container(
-    //           width: double.infinity,
-    //           height: MediaQuery.of(context).size.height / 1.6,
-    //           decoration: const BoxDecoration(
-    //             color: Colors.white,
-    //             borderRadius: BorderRadius.only(
-    //               topRight: Radius.circular(30),
-    //               topLeft: Radius.circular(30),
-    //             ),
-    //           ),
-    //           child: Column(
-    //             children: [
-    //               const SizedBox(
-    //                 height: 5,
-    //               ),
-    //               Icon(
-    //                 Icons.drag_handle_rounded,
-    //                 color: Colors.grey.shade600,
-    //               ),
-    //               const SizedBox(
-    //                 height: 5,
-    //               ),
-    //               Row(
-    //                 mainAxisAlignment: MainAxisAlignment.center,
-    //                 children: [
-    //                   HomeScreeningButton(
-    //                     name: StringConstant.homeStartQuiz,
-    //                     onPressed: () {
-    //                       Navigator.push(
-    //                         context,
-    //                         MaterialPageRoute(
-    //                           builder: (context) {
-    //                             return const QuizUi();
-    //                           },
-    //                         ),
-    //                       );
-    //                     },
-    //                   ),
-    //                   const SizedBox(
-    //                     width: 16,
-    //                   ),
-    //                   HomeConsultationButton(
-    //                     name: StringConstant.homeStartQuiz,
-    //                     onPressed: () async {
-    //                       if (!await checkEligbleTakeQuiz()) {
-    //                         ScaffoldMessenger.of(context).showSnackBar(
-    //                           const SnackBar(
-    //                             backgroundColor: Colors.red,
-    //                             content: Text(
-    //                                 'Lengkapi dulu data pribadi anda di halaman profil'),
-    //                           ),
-    //                         );
-    //                         return;
-    //                       } else {
-    //                         Navigator.push(
-    //                           context,
-    //                           MaterialPageRoute(
-    //                             builder: (context) {
-    //                               return const QuizUi();
-    //                             },
-    //                           ),
-    //                         );
-    //                       }
-    //                     },
-    //                   ),
-    //                 ],
-    //               ),
-    //               SizedBox(
-    //                 height: MediaQuery.of(context).size.height / 25,
-    //               ),
-    //             ],
-    //           ),
-    //         ),
-    //       ],
-    //     ),
-    //   ),
-    // );
   }
 }
