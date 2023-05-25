cdc669bfd46c9843d0a5c8a229b65e2410bff090
fanolans <mailenolan@gmail.com>
2023-05-25 23:07:01 +0700

    refactor folder name import

--- lib/common/utils/providers.dart
+++ lib/common/utils/providers.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_quiz/cubit/auth/auth_cubit.dart';
 import 'package:flutter_quiz/cubit/education/education_cubit.dart';
-import 'package:flutter_quiz/cubit/monitoring/monitoring_cubit.dart';
 import 'package:flutter_quiz/cubit/quiz/quiz_cubit.dart';
 import 'package:flutter_quiz/cubit/quiz_answer/quiz_answer_cubit.dart';
 
@@ -10,6 +9,7 @@ import 'package:flutter_quiz/data/datasources/firebase_auth_datasource.dart';
 import 'package:flutter_quiz/data/datasources/firebase_database_datasource.dart';
 
 import '../../cubit/account/account_cubit.dart';
+import '../../cubit/faq/monitoring_cubit.dart';
 
 class AppProviders extends StatelessWidget {
   const AppProviders({
