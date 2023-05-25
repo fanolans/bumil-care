44f7dda9762fbc1a1d1ee32ec76012ccdce4ac85
fanolans <mailenolan@gmail.com>
2023-05-26 02:35:13 +0700

    create education detail ui

---
+++ lib/ui/pages/education/component/education_ui_detail.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_quiz/ui/pages/education/education_ui.dart';
+
+import '../../../../data/models/education.dart';
+import 'education_ui_image_container.dart';
+
+class EducationDetail extends StatelessWidget {
+  const EducationDetail({
+    Key? key,
+    required this.education,
+  }) : super(key: key);
+
+  final Education education;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              EducationImageContainer(
+                education: education,
+                height: MediaQuery.of(context).size.height / 2.5,
+                width: double.infinity,
+                imageUrl: 'https://placeimg.com/640/480/any',
+              ),
+            ],
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Container(
+                width: double.infinity,
+                margin: EdgeInsetsDirectional.only(
+                    top: MediaQuery.of(context).size.height / 3),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(30),
+                    topRight: Radius.circular(30),
+                  ),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(24),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        education.title,
+                        style: const TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 16,
+                      ),
+                      Text(
+                        education.description,
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const Expanded(
+                child: EducationPage(),
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
